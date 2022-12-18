9c47eef5c15fb81e5960b5bca7f30ac749fa2ca9
shuhaozhen1 <shuhaozhen@gmail.com>
2022-12-19 02:51:03 +0800

    localpoly estimate

---
+++ R/localpoly_fda.R
@@ -0,0 +1,14 @@
+#' the functions for local polynomial estimators for data in the form of (T,Y)
+#'
+#'
+#'
+#'
+#'
+#'
+
+localpoly.ty <- function(data, d=1, domain=c(0,1),
+                         est_t=seq(from= domain[1], to= domain[2], length.out=51)
+)
+{
+
+}

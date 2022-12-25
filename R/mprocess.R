fc4bc431ff2f8c96c9d3e2186f5b4a50dfcba2fb
shuhaozhen1 <shuhaozhen@gmail.com>
2022-12-25 20:13:38 +0800

    kl process

--- R/mprocess.R
+++ R/mprocess.R
@@ -4,16 +4,21 @@
 #'
 #'
 
-
 library(L1pack)
-### Use Fourier basis functions to generate random process
-
+###
+### Kernel functions
+EpaK <- function(x){
+  y <- ifelse (x^2 > 1, 0 ,  3/4*(1-x^2))
+  return(y)
+}
+###
+#### ### Use Fourier basis functions to generate random process
 Laplace.process <- function(x , tvar = 1 , k=100){
   num <- 1:k
   coefx <- as.vector(rmLaplace(n=1,Scatter = diag(sqrt(tvar/(2^(2: (k+1)) ) ) ) ) )
   basis <- sapply(num,function(n,x){
-      (1- (-1)^n)/2 * cos(2* pi * n * x) + (1- (-1)^n)/2 * sin(2* pi * n * x)
-    },x=x)
+    (1- (-1)^n)/2 * cos(2* pi * n * x) + (1- (-1)^n)/2 * sin(2* pi * n * x)
+  },x=x)
   if(length(x) == 1) {
     valueatx <- sum( basis * coefx )
   } else {
@@ -21,8 +26,9 @@ Laplace.process <- function(x , tvar = 1 , k=100){
   }
   return(valueatx)
 }
+####
 
-
+### Transfer matrix
 p <- 3
 transfermatrix <- matrix(0,p,p)
 for(i in 1:p){
@@ -31,18 +37,45 @@ for(i in 1:p){
   }
 }
 
-mi <- 110
 
-re <- 0.01
+### data generation for one trajectory
+datafori <- function(m,sigma=0.1){
+  mcandidate <- (m-2):(m+2)
+  mi <- sample(mcandidate,1, replace = T)
+  tpoints <- sort(runif(mi))
+  rawx <- matrix(replicate(n=p, Laplace.process(tpoints)), nrow = mi)
 
-tpoints <- sort(runif(mi))
+  y0 <- transfermatrix %*% t(rawx)
 
-rawx <- matrix(replicate(n=p, Laplace.process(tpoints)), nrow = mi)
+  y <- y0 + rnorm(mi,0,sigma)
+  return(cbind(tpoints,t(y),rep.int(mi,mi)))
+}
+
+dataforn <- function(n,m,sigma=0.1) {
+  tdata <- replicate(n,datafori(m,sigma))
+  return(tdata)
+}
+
+### data
+
+ty <- dataforn(100,3)
+tdata <- do.call(rbind, ty)
 
 
-y0 <- transfermatrix %*% t(rawx)
+### localpoly
 
-y <- y0 + rnorm(mi,0,re)
+localp <- function(data,t,d=1,h){
+  Tnmt <- sapply(0:d, function(data,t,d){
+    (data[,1]-t)^d
+  }, data=tdata,t=t)
+  Wnmt <- diag(EpaK((tdata[,1]-t)/h) / h / tdata[,p+2])
+  Ynmt <- tdata[,2:(p+1)]
 
+  estd <- (solve(t(Tnmt) %*% Wnmt %*% Tnmt)) %*% (t(Tnmt) %*% Wnmt %*% Ynmt)
+  muhat <- estd[1,]
+  return(muhat)
+}
+
+localp(tdata,t=0.1,h=0.1)
 
 

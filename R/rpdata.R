635c52a7892b48c453fef441606d0f63dfcc6f6d
shuhaozhen1 <shuhaozhen@gmail.com>
2023-01-29 02:17:56 +0800

    Update rpdata.R

--- R/rpdata.R
+++ R/rpdata.R
@@ -5,6 +5,49 @@
 #'
 #'
 #'
+#'
+#'
+
+
+library(foreach)
+library(doParallel)
+### Kernel functions
+EpaK <- function(x){
+  y <- ifelse (x^2 > 1, 0 ,  3/4*(1-x^2))
+  return(y)
+}
+###
+tpoints <- seq(0,1,length.out=21)
+
+mean1 <- function(x){
+  sin(pi*x)
+}
+
+mean2 <- function(x){
+  2*sin(pi*x)
+}
+
+mean3 <- function(x){
+  -sin(pi*x)
+}
+
+mean4 <- function(x){
+  cos(pi*x)
+}
+
+mean5 <- function(x){
+  -cos(pi*x)
+}
+
+meanlist <- list(mean1,mean2,mean3,mean4,mean5)
+
+
+
+
+
+
+
+
 
 ## KL-representation of random process
 rpatx <- function(x , tvar = 1 , k=100, distribution='gaussian'){
@@ -59,27 +102,7 @@ dataforn <- function(n,m,p=5,mean,sigma=0.1) {
 
 ### data simulation
 
-mean1 <- function(x){
-  sin(pi*x)
-}
 
-mean2 <- function(x){
-  2*sin(pi*x)
-}
-
-mean3 <- function(x){
-  -sin(pi*x)
-}
-
-mean4 <- function(x){
-  cos(pi*x)
-}
-
-mean5 <- function(x){
-  -cos(pi*x)
-}
-
-meanlist <- list(mean1,mean2,mean3,mean4,mean5)
 
 ### localpoly
 
@@ -121,32 +144,87 @@ ismootht <- function(t,data,h){
   return(hatit)
 }
 
-a <- dataforn(10,5,mean=meanlist)
 
 ismooth <- function(tpoints,data,h){
   totalsmooth <- sapply(tpoints,ismootht,data,h=h)
   return(totalsmooth)
 }
 
-tpoints <- seq(0,1,length.out=11)
-
-idsmootheddata <- lapply(a,ismooth,tpoints=tpoints,h=0.1)
-
-pt <- 1
-
-idsmootheddatapt <- lapply(idsmootheddata, function(x){
-  t(t(x)/pt)
-})
-
 gaussbs <- function(data){
   bsdata <- lapply(data,function(x){
     rnorm(1)*x
   })
-  bs <- Reduce('+',bsdata)/10
+  bs <- Reduce('+',bsdata)/sqrt(n)
   return(bs)
 }
 
 
-replicate(500, gaussbs(idsmootheddatapt) , simplify = F)
+
+###
+truemean <- t(sapply(meanlist,function(x){x(tpoints)}))
 
 
+n <- 100
+m <- 5
+sigma <- 0.01
+
+registerDoParallel(detectCores()-4)
+
+results <- foreach (rp=1:100, .combine=cbind, .errorhandling = 'remove') %dopar% {
+  ty <- dataforn(n,m,mean=meanlist,sigma = sigma)
+  matrixformty <- do.call(rbind,ty)
+  p <- ncol(matrixformty)-2
+  
+  hcv <- 1.2 * bw.bcv(matrixformty[,1])
+  
+  muhat <- localp(matrixformty, t = tpoints, h=hcv)
+
+  
+  centerdata <- lapply(ty, center, totaldata=matrixformty, h=hcv)
+  
+  Reduce('+',idsmootheddata)/100
+  
+  localp(do.call(rbind,centerdata),t=tpoints,h=hcv)
+    
+  idsmootheddata <- lapply(centerdata,ismooth,tpoints=tpoints,h=hcv)
+  
+  pt <- density(matrixformty[,1], from = 0, to =1, n=21)$y
+  
+  idatapt <- lapply(idsmootheddata, function(x){
+    t(t(x)/pt)
+  })
+  
+  Reduce('+',idsmootheddata)/100
+  sdest <- sqrt(Reduce('+',lapply(idatapt, function(x){x^2}))/n)
+
+  adpidsmootheddata <- lapply(idatapt, function(x){
+    t(t(x)/ t(sdest))
+  })
+  
+  gaussdata <- replicate(2000, gaussbs(adpidsmootheddata) , simplify = F)
+  
+  supdata <- sapply(gaussdata, function(x){
+    max(abs(x))
+  })
+  
+  qhat <- quantile(supdata,0.95)
+  
+  bandup <- muhat + qhat* sdest/sqrt(n)
+  bandlow <- muhat - qhat* sdest/sqrt(n)
+  
+  type1 <- 1 - floor(mean((bandup >= truemean & bandlow <= truemean)))
+  
+  bandwidth <- mean(bandup-bandlow)
+  
+  c(type1, bandwidth)
+}
+
+
+stopImplicitCluster()
+
+
+
+rowMeans(results)
+
+
+(Tnmt) %*% Wnmt %*% Tnmt)
